ce698060f5fa44aa979abd9a5d00b2de2b4ff57a
nekhozhin <nekhozhin@netcracker.com>
2021-02-12 09:52:43 +0400

    q

--- index.js
+++ index.js
@@ -1,230 +1,165 @@
-const express = require('express')
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
+const {includes, lowerCase, random, sample} = require('lodash');
+const express = require('express');
 
 express()
+    .use(express.json())
     .get('/', (req, res) => {
         res.send("OK").status(200);
     })
-    .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+    .post('/alice', (req, res) => {
+        console.log("req", req.body);
+        const {meta, request, version, session} = req.body;
+        console.log(meta);
+        console.log(request);
+        console.log(version);
+        console.log(session);
+        const userUtterance = (request.original_utterance || "").toLowerCase();
+        console.log("userUtterance", userUtterance);
 
-/*
-// API данных (случайные цитаты):
-const api = `http://api.forismatic.com/api/1.0/?method=getQuote&format=json&key=${random(1, 999999)}&lang=ru`;
-// URL страницы для сбора донаций (donation -- в данном случае пожертвования на разработку):
-const donateUrl = 'https://yasobe.ru/na/vui';
-
-
-module.exports = async (req, res) => {
-    // Код для HTTP-ответа:
-    let statusCode = 200;
-
-    // Принимаем только POST-запросы:
-    if (req.method !== "POST") {
-        statusCode = 400;
-        send(res, statusCode, 'Bad Request');
-        return;
-    }
-
-    // API Яндекс.Диалоги:
-    const {meta, request, session, version} = await json(req);
-
-    // Текущая сессия не закрыта:
-    let isEndSession = false;
-
-    // Получаем фразу юзера или надпись с кнопки которую он нажал (и переводим в нижний регистр):
-    const userUtterance = lowerCase(request.original_utterance);
-
-    // Сообщение юзеру (на всякий случай знак пробела, чтобы застраховаться от ошибки в Яндекс.Диалоги, если соощение окажется пустой строкой):
-    let message = ' ';
-
-    // Быстрый ответ (чтобы не делать лишние запросы к стороннему API) на проверочный пинг от Яндекса:
-    if (userUtterance === 'ping') {
-        message = 'ОК';
-        isEndSession = true;
-        send(res, statusCode, {
-            version,
-            session,
-            response: {
-                text: message,
-                end_session: isEndSession
-            }
-        });
-        return;
-    }
-
-    // Проверяем есть ли на устройстве юзера экран (т.е. смартфон или колонка):
-    const hasScreen = typeof meta.interfaces.screen !== "undefined" ? true : false;
-
-    // Получаем массив всех слов из последней фразы юзера:
-    let userWords = [];
-
-    if (request.nlu.tokens.length > 0) {
-        const tokensArr = request.nlu.tokens;
-        for (let i = 0; i < tokensArr.length; i++) {
-            userWords.push(tokensArr[i]);
+        // Быстрый ответ (чтобы не делать лишние запросы к стороннему API) на проверочный пинг от Яндекса:
+        if (userUtterance === 'ping') {
+            res.send({
+                version, session,
+                response: {
+                    text: 'ОК',
+                    end_session: true
+                }
+            }).status(200);
+            return;
         }
-    }
-
-    // Слот для кнопок (саджестов):
-    let buttonSlot = [];
-
-    // Кнопка продолжения (игры):
-    const playButton = {"title": "Продолжай", "hide": true};
-
-    // Кнопка справки:
-    const helpButton = {"title": "Справка", "hide": true};
-
-    // Кнопка донации:
-    const donateButton = {"title": "Поддержи проект", "hide": true, "url": donateUrl};
 
 
-    // Перманентный вопрос к юзеру из серии: "Хотите продолжить?"
-    // С помощью нескольких строковых массивов и функции sample() из библиотеки Lodash, которая
-    // рендомно выбирает элемент массива, создаём вариативность, столь важную для голосовых интерфейсов.
-    // Строковые масивы лучше выносить в отдельный файл, но в данном случае, для наглядности -- всё пишем
-    // в одном файле:
-    const wish = [
+        // Получаем массив всех слов из последней фразы юзера:
+        let userWords = [];
+
+        if (request.nlu.tokens.length > 0) {
+            const tokensArr = request.nlu.tokens;
+            for (let i = 0; i < tokensArr.length; i++) {
+                userWords.push(tokensArr[i]);
+            }
+        }
+
+        // Слот для кнопок (саджестов):
+        let buttonSlot = [];
+        const playButton = {"title": "Продолжай", "hide": true};
+
+
+        // Перманентный вопрос к юзеру из серии: "Хотите продолжить?"
+        const wish = [
             'Хотите',
             'Желаете',
             'Не против'
-        ],
-        know = [
-            'узнать',
-            'оценить',
-            'услышать',
-            'послушать'
-        ],
-        thought = [
-            'ещё одну мысль',
-            'следующую мысль',
-            'ещё одну цитату',
-            'следующую цитату',
-            'ещё одну умную мысль',
-            'ещё одну интересную цитату',
-            'следующую интересную цитату',
-            'мысль ещё одного умного человека'
         ];
+        const know = [
+            'узнать',
+            'услышать',
+        ];
+        const thought = [
+            'следующее задание',
+            'что будет дальше',
+        ];
+        // Формируем перманентный вопрос к юзеру:
+        const prompt = `${sample(wish)} ${sample(know)} ${sample(thought)}?`;
 
-    // Формируем перманентный вопрос к юзеру:
-    const prompt = `${sample(wish)} ${sample(know)} ${sample(thought)}?`;
+        // Создадим также (на этот раз для простоты без вариативности) фразы приветствия, справки, прощания,
+        // а также фразу для ответа на непонятые (т.е. те, которые наш код ещё не обрабатывает) вопросыи юзера:
+        const hello = 'Привет! Поиграем? Скажите "Дальше"';
+        const help = 'Я умею играть в твистер! Чтобы закрыть - скажите: "Выйти"';
+        const bye = 'Спасибо за внимание! До скорой встречи!';
+        const unknown = 'Я не поняла повторите';
 
+        // Теперь стараемся понять юзера. Эту логику также лучше писать в отдельном файле,
+        // поскольку может быть много кода, но в данном весьма упрощённом примере -- пишем здесь.
+        // Будем использовать функции includes() из библиотеки Lodash, которая ищет подстроку.
 
-    // Создадим также (на этот раз для простоты без вариативности) фразы приветствия, справки, прощания,
-    // а также фразу для ответа на непонятые (т.е. те, которые наш код ещё не обрабатывает) вопросыи юзера:
-    const hello = 'Привет! У меня много умных мыслей. Хочешь послушать?';
-    const help = 'Я умею цитировать умных людей. Чтобы слушать - отвечайте положительно на мои вопросы. Чтобы закрыть - скажите: "Нет", "Выйти" "Закрыть"';
-    const bye = 'Спасибо за внимание! До скорой встречи!';
-    const unknown = 'Сейчас я не очень хорошо вас понимаю. Вы можете продолжить, получить справку, или завершить сессию. Каким будет ваш выбор?';
+        // Намерения юзера:
+        let intent;
 
+        // 1. Юзер хочет слушать цитаты (играть -- в нашей терминологии):
+        const playWords = ['дальше', 'продолжить', 'продолжать'];
 
-    // Теперь стараемся понять юзера. Эту логику также лучше писать в отдельном файле,
-    // поскольку может быть много кода, но в данном весьма упрощённом примере -- пишем здесь.
-    // Будем использовать функции includes() из библиотеки Lodash, которая ищет подстроку.
-
-    // Намерения юзера:
-    let intent;
-
-    // 1. Юзер хочет слушать цитаты (играть -- в нашей терминологии):
-    const playWords = ['продолжай', 'продолжить', 'продолжать', 'хочу', 'желаю', 'не против',
-        'да', 'слушать', 'слушаю', 'говори', 'скажи'];
-
-    for (let item of playWords) {
-        if (includes(userUtterance, item)) {
-            intent = 'play';
-            break;
+        for (let item of playWords) {
+            if (includes(userUtterance, item)) {
+                intent = 'play';
+                break;
+            }
         }
-    }
 
-    // 2. Юзер хочет получить справку (на фразы "помощь" и "что ты умеешь" тестируют при модерации навыка):
-    const helpWords = ['справка', 'помощь', 'что ты умеешь'];
+        // 2. Юзер хочет получить справку (на фразы "помощь" и "что ты умеешь" тестируют при модерации навыка):
+        const helpWords = ['справка', 'помощь', 'что ты умеешь'];
 
-    for (let item of helpWords) {
-        if (includes(userUtterance, item)) {
-            intent = 'help';
-            break;
+        for (let item of helpWords) {
+            if (includes(userUtterance, item)) {
+                intent = 'help';
+                break;
+            }
         }
-    }
 
-    // 3. Юзер хочет закрыть навык:
-    const exitWords = ['нет', 'выйти', 'закрыть', 'завершить', 'хватит', 'достаточно'];
+        // 3. Юзер хочет закрыть навык:
+        const exitWords = ['нет', 'выйти', 'закрыть', 'завершить', 'хватит', 'достаточно'];
 
-    for (let item of exitWords) {
-        if (includes(userUtterance, item)) {
-            intent = 'exit';
-            break;
+        for (let item of exitWords) {
+            if (includes(userUtterance, item)) {
+                intent = 'exit';
+                break;
+            }
         }
-    }
 
+        let isEndSession = false;
+        let message;
 
-    // И вот он -- диалог с юзером!:
-    if (!userUtterance) {
-        // Приветствие при запуске:
-        message = hello;
-        // Если у юзера есть экран:
-        if (hasScreen) {
+        // И вот он -- диалог с юзером!:
+        if (!userUtterance) {
+            // Приветствие при запуске:
+            message = hello;
+            // Если у юзера есть экран:
+            //if (hasScreen) {
             // Кнопка "Справка":
             buttonSlot.push(helpButton);
             // Кнопка "Продолжай":
             buttonSlot.push(playButton);
-        }
-    } else if (intent === 'play') {
-        // Определение функции setData() -- в конце кода:
-        message = await setData();
-        if (hasScreen) {
-            // Если значение donateUrl определено -- кнопка донации:
-            if (donateUrl)
-                buttonSlot.push(donateButton);
-            buttonSlot.push(playButton);
-        }
-    } else if (intent === 'help') {
-        message = `${help} ${prompt}`;
-        if (hasScreen) {
-            if (donateUrl)
-                buttonSlot.push(donateButton);
-            buttonSlot.push(playButton);
-        }
-    } else if (intent === 'exit') {
-        message = bye;
-        buttonSlot = [];
-        isEndSession = true;
-    } else {
-        message = unknown;
-        if (hasScreen) {
+            //}
+        } else if (intent === 'play') {
+            // Определение функции setData() -- в конце кода:
+            message = setData();
+        } else if (intent === 'help') {
+            message = `${help} ${prompt}`;
+        } else if (intent === 'exit') {
+            message = bye;
+            buttonSlot = [];
+            isEndSession = true;
+        } else {
+            message = unknown;
             buttonSlot.push(helpButton);
             buttonSlot.push(playButton);
         }
-    }
+
+        console.log("Message", message);
 
 
-    // Ответ Алисе:
-    send(res, statusCode, {
-        version,
-        session,
-        response: {
-            text: message,
-            buttons: buttonSlot,
-            end_session: isEndSession
-        }
-    });
+        // Ответ Алисе:
+        res.send({
+            version,
+            session,
+            response: {
+                text: message,
+                buttons: buttonSlot,
+                end_session: isEndSession
+            }
+        }).status(200);
+    })
+    .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 
-    // Функция, которая получает данные и возвращает отформатированную цитату:
-    async function setData() {
-        let quote;
-        let author;
-        try {
-            // Получение "сырых" данных:
-            const data = await fetch(api);
-            const jData = await data.json();
-            // Получение цитаты и имени автора:
-            quote = jData.quoteText;
-            author = jData.quoteAuthor;
-        } catch (err) {
-            quote = 'Мысль потеряна! Попробуйте ещё раз.';
-            console.error('Fail to fetch data: ' + err);
-        }
-        // Отформатированная цитата:
-        return `${quote}\n${author ? '— ' : ''}${author}\n${prompt}`;
-    }
-};
-*/
+function setData() {
+    const a = ['Левая', 'Правая'];
+    const b = ['рука', 'нога'];
+    const c = ['на'];
+    const d = ['желтый', 'красный', 'синий', 'зеленый'];
+    const e = ['круг'];
+    const answer = `${sample(a)} ${sample(b)} ${sample(c)} ${sample(d)} ${sample(e)}`;
+    return answer;
+}
+
+
